f2c152e1d6cd86f193e2698d5c9978bf8b484a5f
itc-s24011 <s24011@std.it-college.ac.jp>
2024-06-17 15:48:17 +0900

    add def2.py

---
+++ def2.py
@@ -0,0 +1,11 @@
+# s24011
+# 関数で消費税を計算するコード
+#
+
+def postTaxprice(price):
+    ans = price * 1.1
+    return ans
+
+print(postTaxprice(120),"円")
+print(postTaxprice(120),"円")
+print(postTaxprice(120),"円")
